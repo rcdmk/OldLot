29119aef9161bd814fd1f9c1af7837bf139e2305
rcdmk <rcdmk@dtavares.com>
2016-06-26 20:48:02 -0300

    Added the domain services

---
+++ OldLot.Dominio/Servicos/ServicoBase.cs
@@ -0,0 +1,58 @@
+﻿using OldLot.Dominio.Interfaces.Repositorios;
+using OldLot.Dominio.Interfaces.Servicos;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace OldLot.Dominio.Servicos
+{
+    public class ServicoBase<TEntity> : IDisposable, IServicoBase<TEntity> where TEntity : class
+    {
+        protected readonly IRepositorioBase<TEntity> repositorio;
+
+        public ServicoBase(IRepositorioBase<TEntity> repositorio)
+        {
+            this.repositorio = repositorio;
+        }
+
+        public void Incluir(TEntity entidade)
+        {
+            repositorio.Incluir(entidade);
+        }
+
+        public void Atualizar(TEntity entidade)
+        {
+            repositorio.Atualizar(entidade);
+        }
+
+        public void Excluir(TEntity entidade)
+        {
+            repositorio.Excluir(entidade);
+        }
+
+        public void ExcluirPorId(int id)
+        {
+            repositorio.ExcluirPorId(id);
+        }
+
+        public TEntity ObterPorId(int id)
+        {
+            return repositorio.ObterPorId(id);
+        }
+
+        public IEnumerable<TEntity> ObterTodos()
+        {
+            return repositorio.ObterTodos();
+        }
+
+        public IEnumerable<TEntity> ObterLista(Expression<Func<TEntity, bool>> filtro)
+        {
+            return repositorio.ObterLista(filtro);
+        }
+
+        public void Dispose()
+        {
+            repositorio.Dispose();
+        }
+    }
+}
---
+++ OldLot.Dominio/Servicos/ServicoFabricante.cs
@@ -0,0 +1,16 @@
+﻿using OldLot.Dominio.Entidades;
+using OldLot.Dominio.Interfaces.Repositorios;
+using OldLot.Dominio.Interfaces.Servicos;
+
+namespace OldLot.Dominio.Servicos
+{
+    public class ServicoFabricante : ServicoBase<Fabricante>, IServicoFabricante
+    {
+        private readonly IRepositorioFabricante repositorioFabricante;
+
+        public ServicoFabricante(IRepositorioFabricante repositorio) : base(repositorio)
+        {
+            this.repositorioFabricante = repositorio;
+        }
+    }
+}
