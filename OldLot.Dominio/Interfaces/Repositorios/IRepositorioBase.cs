22dd9232fc922e95f27b3741fedf8b0e339a6dd9
rcdmk <rcdmk@dtavares.com>
2016-06-26 18:33:14 -0300

    Added the repository interfaces

---
+++ OldLot.Dominio/Interfaces/Repositorios/IRepositorioBase.cs
@@ -0,0 +1,22 @@
+﻿using OldLot.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OldLot.Dominio.Interfaces.Repositorios
+{
+    public interface IRepositorioBase<TEntity> : IDisposable where TEntity : EntidadeBase
+    {
+        TEntity ObterPorId(int id);
+        ICollection<TEntity> ObterTodos();
+        ICollection<TEntity> ObterLista(Expression<Func<TEntity, bool>> filtro);
+
+        void Incluir(TEntity entidade);
+        void Atualizar(TEntity entidade);
+        void Excluir(TEntity entidade);
+        void ExcluirPorId(int id);
+    }
+}
