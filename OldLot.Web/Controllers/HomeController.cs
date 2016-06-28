92e4c9550ec30f8bfdc3eddecd33c37c64f81c6b
rcdmk <rcdmk@dtavares.com>
2016-06-28 01:33:16 -0300

    Created the home and collection list

--- OldLot.Web/Controllers/HomeController.cs
+++ OldLot.Web/Controllers/HomeController.cs
@@ -1,28 +1,12 @@
-﻿using AutoMapper;
-using OldLot.Aplicacao;
-using OldLot.Web.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace OldLot.Web.Controllers
 {
     public class HomeController : Controller
     {
-        private ServicoAplicacaoVeiculo servico;
-
-        public HomeController(ServicoAplicacaoVeiculo servico)
-        {
-            this.servico = servico;
-        }
-
         public ActionResult Index()
         {
-            var veiculos = Mapper.Map<IEnumerable<VeiculoViewModel>>(servico.ObterTodos());
-
-            return View(veiculos);
+            return View();
         }
     }
 }
---
+++ OldLot.Web/Controllers/VeiculoController.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using OldLot.Aplicacao;
+using OldLot.Web.ViewModels;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace OldLot.Web.Controllers
+{
+    public class VeiculoController : Controller
+    {
+        private ServicoAplicacaoVeiculo servico;
+
+        public VeiculoController(ServicoAplicacaoVeiculo servico)
+        {
+            this.servico = servico;
+        }
+
+        // GET: Veiculo
+        public ActionResult Index()
+        {
+            var veiculos = Mapper.Map<IEnumerable<VeiculoViewModel>>(servico.ObterTodos());
+
+            return View(veiculos);
+        }
+    }
+}
